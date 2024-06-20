8c78b45192d914e1df20df5232fcaa98bd8afa8c
kaichaosun <kaichaosuna@gmail.com>
2024-06-20 11:15:57 +0800

    shadcn ui init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './globals.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button>Hello</Button>
     </>
   )
 }
